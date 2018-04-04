3feecbe861cfa610f02c2447dcdddd39e76385e1
Bin Wu <wub@google.com>
2018-04-04 14:42:42 -0400

    Add no-op "implementation" for QUIC platform apis: quic_flags_lib and quic_flag_utils_lib.

---
+++ source/common/quic/platform/impl/quic_flag_utils_impl.h
@@ -0,0 +1,11 @@
+#ifndef GFE_QUIC_PLATFORM_IMPL_QUIC_FLAG_UTILS_IMPL_H_
+#define GFE_QUIC_PLATFORM_IMPL_QUIC_FLAG_UTILS_IMPL_H_
+
+// TODO: Implement quic_flag_utils_impl.h
+#define QUIC_FLAG_COUNT_IMPL(flag) do {} while (0)
+#define QUIC_FLAG_COUNT_N_IMPL(flag, instance, total) do {} while (0)
+
+#define QUIC_CODE_COUNT_IMPL(name) do {} while (0)
+#define QUIC_CODE_COUNT_N_IMPL(name, instance, total) do {} while (0)
+
+#endif  // GFE_QUIC_PLATFORM_IMPL_QUIC_FLAG_UTILS_IMPL_H_
---
+++ source/common/quic/platform/impl/quic_flags_impl.h
@@ -0,0 +1,14 @@
+#ifndef GFE_QUIC_PLATFORM_IMPL_QUIC_FLAGS_IMPL_H_
+#define GFE_QUIC_PLATFORM_IMPL_QUIC_FLAGS_IMPL_H_
+
+// TODO: Implement quic_flags_impl.h.
+#define GetQuicReloadableFlagImpl(flag) false
+#define SetQuicReloadableFlagImpl(flag, value) do {} while (0)
+
+#define GetQuicRestartFlagImpl(flag) false
+#define SetQuicRestartFlagImpl(flag, value) do {} while (0)
+
+#define GetQuicFlagImpl(flag) false
+#define SetQuicFlagImpl(flag, value) do {} while (0)
+
+#endif  // GFE_QUIC_PLATFORM_IMPL_QUIC_FLAGS_IMPL_H_
